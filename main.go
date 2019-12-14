8a595c8dc9e465aa4b022b9402adbeff15063d6c
Hoxwo <arcturusmuse@gmail.com>
2019-12-13 20:48:01 -0500

    stuff actually showing up

--- main.go
+++ main.go
@@ -1,29 +1,23 @@
 package main
 
-import "fmt"
-import "time"
+//import "image"
+import "log"
 import "math/rand"
-import "strings"
-import planet "many_moons/planet"
-import ship "many_moons/ship"
+import "time"
+//import "strings"
+//import planet "many_moons/planet"
+//import ship "many_moons/ship"
 import civilization "many_moons/civilization"
 import ui "github.com/gizak/termui/v3"
+import "github.com/gizak/termui/v3/widgets"
 
 func main() {
 	// set the current year to 0
-	currentyear := 0
+	// currentyear := 0
 	//for randos
 	rand.Seed(time.Now().UTC().UnixNano())
 	// array of civilizations 
 	civilizations := make([]*civilization.Civilization, 0)
-	// array of planets
-	coinPrices := make(map[string]float64)
-	//index of selected planet
-	selected := 0
-	// current state
-	state := 1 // 1 - watching, 2 - buying, 3 - selling
-	//last player quantity input
-	lastPlayerInput := 0
 
 	//All colors
 	//termui.ColorCyan
@@ -48,23 +42,51 @@ func main() {
 
 
 	//select first planet
-	selected = 0
+	//selected = 0
 
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 	
-	planetmap := ui.NewCanvas()
-	planetmap.SetRect(0, 78, 0, 34)
-	planetmap.SetLine(image.Pt(0, 0), image.Pt(10, 20), ui.ColorWhite)
+	//planetmap := ui.NewCanvas()
+	//planetmap.SetRect(0, 0, 78, 34)
+	//planetmap.SetLine(image.Pt(0, 0), image.Pt(10, 20), ui.ColorWhite)
 
-	datapane := ui.NewCanvas()
-	datapane.SetRect(78, 126, 0, 34)
-	datapane.SetLine(image.Pt(0, 0), image.Pt(10, 20), ui.ColorWhite)
+	//datapane := ui.NewCanvas()
+	//datapane.SetRect(78, 0, 126, 34)
+	//datapane.SetLine(image.Pt(0, 0), image.Pt(10, 20), ui.ColorWhite)
 
+	//ui.Render(planetmap, datapane)
 
-	ui.Render(planetmap, datapane)
+	p0 := widgets.NewParagraph()
+	p0.Text = "Borderless Text"
+	p0.SetRect(0, 0, 20, 5)
+	p0.Border = false
+
+	p1 := widgets.NewParagraph()
+	p1.Title = "标签"
+	p1.Text = "你好，世界。"
+	p1.SetRect(20, 0, 35, 5)
+
+	p2 := widgets.NewParagraph()
+	p2.Title = "Multiline"
+	p2.Text = "Simple colored text\nwith label. It [can be](fg:red) multilined with \\n or [break automatically](fg:red,fg:bold)"
+	p2.SetRect(0, 5, 35, 10)
+	p2.BorderStyle.Fg = ui.ColorYellow
+
+	p3 := widgets.NewParagraph()
+	p3.Title = "Auto Trim"
+	p3.Text = "Long text with label and it is auto trimmed."
+	p3.SetRect(0, 10, 40, 15)
+
+	p4 := widgets.NewParagraph()
+	p4.Title = "Text Box with Wrapping"
+	p4.Text = "Press q to QUIT THE DEMO. [There](fg:blue,mod:bold) are other things [that](fg:red) are going to fit in here I think. What do you think? Now is the time for all good [men to](bg:blue) come to the aid of their country. [This is going to be one really really really long line](fg:green) that is going to go together and stuffs and things. Let's see how this thing renders out.\n    Here is a new paragraph and stuffs and things. There should be a tab indent at the beginning of the paragraph. Let's see if that worked as well."
+	p4.SetRect(40, 0, 70, 20)
+	p4.BorderStyle.Fg = ui.ColorBlue
+
+	ui.Render(p0, p1, p2, p3, p4)
 
 	uiEvents := ui.PollEvents()
 	for {
